00d1eb210d886db70c26e5796922cbf46c1bbaa8
allanhull <allanpaulhull@outlook.com>
2023-07-31 21:36:54 -0500

    2023JUL31 Commit #1

--- LPAR/NOTES.swift
+++ LPAR/NOTES.swift
@@ -10,6 +10,9 @@
 // APP NOTES
 //
 
+// 2023JUL31
+// remove .usdz Teapot, toy_biplane and toy_robot_vintage
+
 // 2023JUL30
 // Commit v2.4 to githug.com
 // Modify .usdz files IA_MIL_pow, IA_SPC_basic, NE_ORG_unl (refernece Pages HOWTO_usdz)
